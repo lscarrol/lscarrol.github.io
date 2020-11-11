7b4f69cb731a9f941cdf989cd1fc675bf478f82d
Liam Carroll <lscarrol@buffalo.edu>
2020-11-11 15:35:28 -0500

    kaya test

---
+++ js/script.js
@@ -0,0 +1,343 @@
+function architectur(x) {
+  if (x.matches) { // If media query matches
+	  document.getElementById("arch").innerHTML = "/architectur";
+  } else {
+	  document.getElementById("arch").innerHTML = "/architecture";
+  }
+}
+
+var x = window.matchMedia("(max-width: 549px)")
+architectur(x) // Call listener function at run time
+x.addListener(architectur) // Attach listener function on state changes
+
+    function architectu(x) {
+      if (x.matches) { // If media query matches
+        document.getElementById("arch").innerHTML = "/architectu";
+      } else {
+        document.getElementById("arch").innerHTML = "/architecture";
+      }
+    }
+
+    var x = window.matchMedia("(max-width: 547px)")
+    architectu(x) // Call listener function at run time
+    x.addListener(architectu) // Attach listener function on state changes
+
+        function architect(x) {
+          if (x.matches) { // If media query matches
+            document.getElementById("arch").innerHTML = "/architect";
+          } else {
+            document.getElementById("arch").innerHTML = "/architecture";
+          }
+        }
+
+        var x = window.matchMedia("(max-width: 545px)")
+        architect(x) // Call listener function at run time
+        x.addListener(architect) // Attach listener function on state changes
+
+            function architec(x) {
+              if (x.matches) { // If media query matches
+                document.getElementById("arch").innerHTML = "/architec";
+              } else {
+                document.getElementById("arch").innerHTML = "/architecture";
+              }
+            }
+
+            var x = window.matchMedia("(max-width: 543px)")
+            architec(x) // Call listener function at run time
+            x.addListener(architec) // Attach listener function on state changes
+
+                function archite(x) {
+                  if (x.matches) { // If media query matches
+                    document.getElementById("arch").innerHTML = "/archite";
+                  } else {
+                    document.getElementById("arch").innerHTML = "/architecture";
+                  }
+                }
+
+                var x = window.matchMedia("(max-width: 541px)")
+                archite(x) // Call listener function at run time
+                x.addListener(archite) // Attach listener function on state changes
+
+                    function archit(x) {
+                      if (x.matches) { // If media query matches
+                        document.getElementById("arch").innerHTML = "/archit";
+                      } else {
+                        document.getElementById("arch").innerHTML = "/architecture";
+                      }
+                    }
+
+                    var x = window.matchMedia("(max-width: 539px)")
+                    archit(x) // Call listener function at run time
+                    x.addListener(archit) // Attach listener function on state changes
+
+                        function archi(x) {
+                          if (x.matches) { // If media query matches
+                            document.getElementById("arch").innerHTML = "/archi";
+                          } else {
+                            document.getElementById("arch").innerHTML = "/architecture";
+                          }
+                        }
+
+                        var x = window.matchMedia("(max-width: 537px)")
+                        archi(x) // Call listener function at run time
+                        x.addListener(archi) // Attach listener function on state changes
+
+                            function arch(x) {
+                              if (x.matches) { // If media query matches
+                                document.getElementById("arch").innerHTML = "/arch";
+                              } else {
+                                document.getElementById("arch").innerHTML = "/architecture";
+                              }
+                            }
+
+                            var x = window.matchMedia("(max-width: 535px)")
+                            arch(x) // Call listener function at run time
+                            x.addListener(arch) // Attach listener function on state changes
+
+
+function photograph(x) {
+  if (x.matches) { // If media query matches
+	  document.getElementById("photo").innerHTML = " /photograph";
+  } else {
+	  document.getElementById("photo").innerHTML = " /photography";
+  }
+}
+
+var x = window.matchMedia("(max-width: 621px)")
+photograph(x) // Call listener function at run time
+x.addListener(photograph) // Attach listener function on state changes
+
+    function photograp(x) {
+      if (x.matches) { // If media query matches
+        document.getElementById("photo").innerHTML = " /photograp";
+      } else {
+        document.getElementById("photo").innerHTML = " /photography";
+      }
+    }
+
+    var x = window.matchMedia("(max-width: 618px)")
+    photograp(x) // Call listener function at run time
+    x.addListener(photograp) // Attach listener function on state changes
+
+
+        function photogra(x) {
+          if (x.matches) { // If media query matches
+            document.getElementById("photo").innerHTML = " /photogra";
+          } else {
+            document.getElementById("photo").innerHTML = " /photography";
+          }
+        }
+
+        var x = window.matchMedia("(max-width: 616px)")
+        photogra(x) // Call listener function at run time
+        x.addListener(photogra) // Attach listener function on state changes
+
+            function photogr(x) {
+              if (x.matches) { // If media query matches
+                document.getElementById("photo").innerHTML = " /photogr";
+              } else {
+                document.getElementById("photo").innerHTML = " /photography";
+              }
+            }
+
+            var x = window.matchMedia("(max-width: 614px)")
+            photogr(x) // Call listener function at run time
+            x.addListener(photogr) // Attach listener function on state changes
+
+                function photog(x) {
+                  if (x.matches) { // If media query matches
+                    document.getElementById("photo").innerHTML = " /photog";
+                  } else {
+                    document.getElementById("photo").innerHTML = " /photography";
+                  }
+                }
+
+                var x = window.matchMedia("(max-width: 612px)")
+                photog(x) // Call listener function at run time
+                x.addListener(photog) // Attach listener function on state changes
+
+                    function photos(x) {
+                      if (x.matches) { // If media query matches
+                        document.getElementById("photo").innerHTML = " /photos";
+                      } else {
+                        document.getElementById("photo").innerHTML = " /photography";
+                      }
+                    }
+
+                    var x = window.matchMedia("(max-width: 610px)")
+                    photos(x) // Call listener function at run time
+                    x.addListener(photos) // Attach listener function on state changes
+
+
+
+function ninein(x) {
+document.getElementById("titlecard").innerHTML = "Big Box Decommission";
+document.getElementById("datecard").innerHTML = "Spring 2020";
+  }
+
+function nineout(x) {
+document.getElementById("titlecard").innerHTML = "Some Projects";
+document.getElementById("datecard").innerHTML = "c. 2020";
+  }
+
+
+
+function threein(x) {
+document.getElementById("titlecard").innerHTML = "Casual Boathouse";
+document.getElementById("datecard").innerHTML = "Spring 2019";
+  }
+
+function threeout(x) {
+document.getElementById("titlecard").innerHTML = "Some Projects";
+document.getElementById("datecard").innerHTML = "c. 2020";
+  }
+
+
+function sevenin(x) {
+document.getElementById("titlecard").innerHTML = "Manhattanisms";
+document.getElementById("datecard").innerHTML = "Summer 2016";
+  }
+
+function sevenout(x) {
+document.getElementById("titlecard").innerHTML = "Some Projects";
+document.getElementById("datecard").innerHTML = "c. 2020";
+  }
+
+
+function fivein(x) {
+document.getElementById("titlecard").innerHTML = "Invisible Cities";
+document.getElementById("datecard").innerHTML = "Fall 2017";
+  }
+
+function fiveout(x) {
+document.getElementById("titlecard").innerHTML = "Some Projects";
+document.getElementById("datecard").innerHTML = "c. 2020";
+  }
+
+
+function tenin(x) {
+document.getElementById("titlecard").innerHTML = "Salt Box";
+document.getElementById("datecard").innerHTML = "Fall 2019";
+  }
+
+function tenout(x) {
+document.getElementById("titlecard").innerHTML = "Some Projects";
+document.getElementById("datecard").innerHTML = "c. 2020";
+  }
+
+
+function twoin(x) {
+document.getElementById("titlecard").innerHTML = "Transparency i";
+document.getElementById("datecard").innerHTML = "Fall 2018";
+  }
+
+function twoout(x) {
+document.getElementById("titlecard").innerHTML = "Some Projects";
+document.getElementById("datecard").innerHTML = "c. 2020";
+  }  
+
+
+
+function fourin(x) {
+document.getElementById("titlecard").innerHTML = "Casa Gilardi";
+document.getElementById("datecard").innerHTML = "Fall 2017";
+  }
+
+function fourout(x) {
+document.getElementById("titlecard").innerHTML = "Some Projects";
+document.getElementById("datecard").innerHTML = "c. 2020";
+  }  
+
+
+function sixin(x) {
+document.getElementById("titlecard").innerHTML = "Transparency ii";
+document.getElementById("datecard").innerHTML = "Fall 2018";
+  }
+
+function sixout(x) {
+document.getElementById("titlecard").innerHTML = "Some Projects";
+document.getElementById("datecard").innerHTML = "c. 2020";
+  }  
+
+
+
+  
+function unoin(x) {
+document.getElementById("titlecard").innerHTML = "Wavy";
+document.getElementById("datecard").innerHTML = "";
+  }
+
+function unoout(x) {
+document.getElementById("titlecard").innerHTML = "Some Photos";
+document.getElementById("datecard").innerHTML = "";
+  }
+
+function dosin(x) {
+document.getElementById("titlecard").innerHTML = "Rocky";
+document.getElementById("datecard").innerHTML = "";
+  }
+
+function dosout(x) {
+document.getElementById("titlecard").innerHTML = "Some Photos";
+document.getElementById("datecard").innerHTML = "";
+  }
+
+function tresin(x) {
+document.getElementById("titlecard").innerHTML = "Sandy";
+document.getElementById("datecard").innerHTML = "";
+  }
+
+function tresout(x) {
+document.getElementById("titlecard").innerHTML = "Some Photos";
+document.getElementById("datecard").innerHTML = "";  
+  }
+
+function cuatroin(x) {
+document.getElementById("titlecard").innerHTML = "Grassy";
+document.getElementById("datecard").innerHTML = "";
+  }
+
+function cuatroout(x) {
+document.getElementById("titlecard").innerHTML = "Some Photos";
+document.getElementById("datecard").innerHTML = "";  
+  }
+
+function cincoin(x) {
+document.getElementById("titlecard").innerHTML = "Dark";
+document.getElementById("datecard").innerHTML = "";
+  }
+
+function cincoout(x) {
+document.getElementById("titlecard").innerHTML = "Some Photos";
+document.getElementById("datecard").innerHTML = "";
+  }
+
+function seisin(x) {
+document.getElementById("titlecard").innerHTML = "Cloudy";
+document.getElementById("datecard").innerHTML = "";
+  }
+
+function seisout(x) {
+document.getElementById("titlecard").innerHTML = "Some Photos";
+document.getElementById("datecard").innerHTML = "";
+  }
+
+function sietein(x) {
+document.getElementById("titlecard").innerHTML = "City";
+document.getElementById("datecard").innerHTML = "";
+  }
+
+function sieteout(x) {
+document.getElementById("titlecard").innerHTML = "Some Photos";
+document.getElementById("datecard").innerHTML = "";  
+  }
+
+function ochoin(x) {
+document.getElementById("titlecard").innerHTML = "Water";
+document.getElementById("datecard").innerHTML = "";
+  }
+
+function ochoout(x) {
+document.getElementById("titlecard").innerHTML = "Some Photos";
+document.getElementById("datecard").innerHTML = "";  
+  }
